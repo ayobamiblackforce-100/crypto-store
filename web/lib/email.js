e183bbd4f774a611d20a183c2e6fcde1af920146
Ayobami Blackforce <ayobamiblackforce@gmail.com>
2026-03-11 09:07:24 -0400

    Create email.js

---
+++ web/lib/email.js
@@ -0,0 +1,16 @@
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export async function sendEmail({ to, subject, html }) {
+  try {
+    await resend.emails.send({
+      from: "CryptoStore <no-reply@yourdomain.com>",
+      to,
+      subject,
+      html
+    });
+  } catch (err) {
+    console.error("Email error:", err);
+  }
+}
